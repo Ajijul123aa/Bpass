c11cc69d6d72557babb396eb8fe5387e29a8c576
AJIJUL HASAN JIBON <68082929+Ajijul123aa@users.noreply.github.com>
2022-06-26 18:34:50 +0600

    Update Bpass.py

--- Bpass.py
+++ Bpass.py
@@ -19,7 +19,7 @@ def fuck():
 	print("\033[96;1m [1] SSB VR: 7.6.8 \033[96;1m[BYPASS]		")
 	print("\033[93;1m [2] SSB VR: 8.0.0 \033[96;1m[BYPASS]	")
 	print("\033[93;1m [3] James VR:0.3\033[96;1m[BYPASS By Jibon]	")
-	print("\033[93;1m [4]NIKI DUMP\033[96;1m[Public NO BYPASS]	")
+	print("\033[93;1m [4]Fcpromax\033[96;1m[Crack]	")
 	print("")
 	xx= input(" [•] CHOICE : ")
 	if xx in ["1","01"]:
@@ -33,7 +33,7 @@ def fuck():
 	os.system('python j.py')
 	if xx in ["4","04"]:
 		time.sleep(1.5)
-	os.system('python DUMP-ID')
+	os.system('python fcpromax.py')
 	
 
 fuck()
